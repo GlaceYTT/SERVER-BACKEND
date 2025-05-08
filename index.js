a750592e53db41656e23060d20831c71f7dabf81
GlaceYTT <official.glaceyt@gmail.com>
2025-05-08 18:24:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 10000;
 const expectedCommandsCount = 94;
 let serverOnline = true;
 
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+}));
+
 app.use(express.json());
 
 // Endpoint to check server status
